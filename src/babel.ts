8f5afa93c2c3fdb5323d7de27604fa202a344553
Hikari Hayashi <rev.hikari@gmail.com>
2023-01-24 15:15:16 +0800

    Re-export babel stuffs

---
+++ src/babel.ts
@@ -0,0 +1,4 @@
+export { default as generate } from "@babel/generator";
+export { parse, parseExpression } from "@babel/parser";
+export { default as template } from "@babel/template";
+export * as t from "@babel/types";
