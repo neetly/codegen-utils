36fc1df44f101d4e7c54b04fea0910c12483bfe4
Hikari Hayashi <rev.hikari@gmail.com>
2023-01-24 15:43:15 +0800

    Add fs stuffs

---
+++ src/fs.ts
@@ -0,0 +1,26 @@
+import * as fs from "node:fs/promises";
+import { dirname } from "node:path";
+
+import * as prettier from "prettier";
+
+const readFile = async (path: string) => {
+  return fs.readFile(path, { encoding: "utf-8" });
+};
+
+const writeFile = async (path: string, content: string) => {
+  await fs.mkdir(dirname(path), { recursive: true });
+  await fs.writeFile(
+    path,
+    prettier.format(content, {
+      filepath: path,
+      ...(await prettier.resolveConfig(path)),
+    }),
+    { encoding: "utf-8" },
+  );
+};
+
+const removeDirectory = async (path: string) => {
+  await fs.rm(path, { recursive: true, force: true });
+};
+
+export { readFile, removeDirectory, writeFile };
